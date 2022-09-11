51e1bb52ab8716dbc51a5d4abf10970ee0373515
Eugene Chew <echew140899@gmail.com>
2022-09-11 21:44:46 +0800

    Updates

--- Eugene/scanning.py
+++ Eugene/scanning.py
@@ -1,7 +1,37 @@
 
+import socket
+import subprocess
+import sys
+from datetime import datetime
+import nmap
+
+IP_ADDRESS = "8.8.8.8"
+
 
 def main():
     print("hi")
+    # target = socket.gethostbyname(sys.argv[1])
+    target = IP_ADDRESS
+    try:
+    # will scan ports between 1 to 65,535
+        for port in range(1, 65535):
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            socket.setdefaulttimeout(1)
+            # returns an error indicator
+            result = s.connect_ex((target, port))
+            if result == 0:
+                print("Port {} is open".format(port))
+            s.close()
 
+    except KeyboardInterrupt:
+            print("\n Exiting Program !!!!")
+            sys.exit()
+    except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+    except socket.error:
+        print("\ Server not responding !!!!")
+    sys.exit()
 
-main()
+if __name__ == "__main__":
+    main()
