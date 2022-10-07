5d6a47723ebd0c1c36f4e495026df0014725a115
Eugene Chew <echew140899@gmail.com>
2022-10-07 15:48:42 +0800

    Update cleaning.py

--- Eugene/cleaning.py
+++ Eugene/cleaning.py
@@ -1,13 +1,9 @@
 import pandas as pd
-import datetime
 
 
-def convert_timestamp(dataframe):
-    for timestamp in dataframe['@timestamp']:
-        old_timestamp = timestamp
-        timestamp = timestamp.replace(',', '').split(' @')
-        timestamp[0] = str(datetime.datetime.strptime(timestamp[0], '%b %d %Y')).split(' ')[0]
-        timestamp = ' '.join(timestamp)
+def sort_values(dataframe, column_header):  # sort data frame by a column
+    dataframe.sort_values(by=column_header, ascending=True)
+    return dataframe
 
 
 def remove_null_columns(dataframe):
@@ -15,19 +11,15 @@ def remove_null_columns(dataframe):
     return dataframe
 
 
+def detect_null_data(dataframe):
+    pass
+
+
 def main():
     df = pd.read_csv(r'port_scan_logs/sorted.csv')
     df = remove_null_columns(df)
     df.to_csv('port_scan_logs/cleaned.csv')
-    # print(df)
-    # columns = df.columns.values
-    # print(columns)
-    # columns['destination.port'] = sorted(columns['destination.port'])
-    # new_df = df.loc[:, df.columns != "@timestamp"]
-    # new_df = new_df.sort_values(by="destination.port", ascending=True)
-    # print(new_df)
 
-    # print(type(df['@timestamp'][0]))
     # df.sort_values("timestamp")
     # pd.to_datetime('@timestamp', format="%b %d %Y %H:%M:%S.%f").sort_values()
     # convert_timestamp(df)
