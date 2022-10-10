1a44457843b05cbb854e1b07e46443a614216b6c
Eugene Chew <echew140899@gmail.com>
2022-10-10 15:45:28 +0800

    Update cleaning.py

--- Eugene/cleaning.py
+++ Eugene/cleaning.py
@@ -2,8 +2,6 @@ import pandas as pd
 import re
 import ipaddress
 import datetime
-from dateutil import parser
-import numpy as np
 
 PORT_REGEX = \
     '^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$'
