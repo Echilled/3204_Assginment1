0942a6d8e1a055b4a86a1871f3682418b25063cf
Eugene Chew <echew140899@gmail.com>
2022-10-16 11:47:58 +0800

    Update firewall_logs_cleaning.py

--- Eugene/firewall_logs_cleaning.py
+++ Eugene/firewall_logs_cleaning.py
@@ -19,8 +19,7 @@ def get_columns_with_all_single_values(dataframe):  # remove columns with the sa
     for column in dataframe.columns:
         if (dataframe[column] == dataframe[column][0]).all():
             same_value_columns.append(column)
-    print(same_value_columns)
-    # return same_value_columns
+    return same_value_columns
 
 
 def output_to_csv(dataframe):
@@ -35,7 +34,7 @@ def format_data(file):
         engine='python',
         na_values='-',
         header=None,
-        usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+        usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17],
         names=headers,
     )
     df = df.iloc[4:]
@@ -44,7 +43,9 @@ def format_data(file):
 
 def drop_redundant_cols(dataframe):
     if get_columns_with_all_single_values(dataframe):
-        print("hi")
+        for column in get_columns_with_all_single_values(dataframe):
+            print(column)
+            dataframe.drop(column, axis=1, inplace=True)
     else:
         print("nothing")
 
@@ -53,6 +54,8 @@ def main():
     df = format_data(PATH)
     df.reset_index(drop=True, inplace=True)
     drop_redundant_cols(df)
+    print(df)
+    output_to_csv(df)
 
 
 if __name__ == "__main__":
