5ca1f3bf43dc964b7e9fa30500c4d764710cc756
Eugene Chew <echew140899@gmail.com>
2022-10-16 10:50:53 +0800

    Update firewall_logs_cleaning.py

--- Eugene/firewall_logs_cleaning.py
+++ Eugene/firewall_logs_cleaning.py
@@ -4,18 +4,33 @@ import pandas as pd
 PATH = 'Webserver_logs/fw_logs.txt'
 
 
-def main():
+def get_headers(file):
+    f = open(file, "r")
+    for line in f:
+        if line.startswith("#Fields:"):
+            line = line.strip()
+            headers = line.split(' ')
+    headers.pop(0)
+    return headers
+
+
+def format_data(file):
+    headers = get_headers(file)
     df = pd.read_csv(
         PATH,
         sep=r'\s(?=(?:[^"]*"[^"]*")*[^"]*$)(?![^\[]*\])',
         engine='python',
         na_values='-',
         header=None,
-        usecols=[0, 3, 4, 5, 6, 7, 8],
-        names=['date', 'time', 'action', 'protocol', 'src-ip', 'dst-ip', 'src-port', 'dst-port', 'size', 'tcpflags',
-               'tcpsyn', 'tcpack', 'tcpwin', 'icmptype', 'icmpcode', 'info', 'path'],
-       )
+        usecols=[0, 1, 2, 3, 4, 5, 6, 7, 8],
+        names=headers,
+    )
+    df = df.iloc[4:]
     print(df)
+
+
+def main():
+    format_data(PATH)
 
 
 if __name__ == "__main__":
