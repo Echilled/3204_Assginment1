fed39f159d1e24acbbd972fe72827eae148bbc74
Eugene Chew <echew140899@gmail.com>
2022-10-21 14:10:13 +0800

    Update passive.py

--- Eugene/passive.py
+++ Eugene/passive.py
@@ -1,14 +1,11 @@
-import whois
-from nslookup import Nslookup
+
 import re
 from requests_html import HTMLSession
+from websockets import client
 import html
-
-domain = "example.com"
 
 
 def email_extractor(url):
-	url = "https://www.generatormix.com/random-email-addresses"
 	EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
 	session = HTMLSession()
 	r = session.get(url)
@@ -18,14 +15,7 @@ def email_extractor(url):
 
 
 def main():
-	# print("hi")
-	# w = whois.whois(domain)
-	# # print(w)
-	#
-	# dns_query = Nslookup()
-	# ips_record = dns_query.dns_lookup(domain)
-	# print(ips_record.response_full, ips_record.answer)
-	email_extractor("https://www.randomlists.com/email-addresses")
+	email_extractor("http://192.168.91.1/")
 
 
 if __name__ == "__main__":
