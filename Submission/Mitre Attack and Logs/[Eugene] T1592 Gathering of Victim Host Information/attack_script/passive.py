a4ad859d3fe4fba0b1121261146e68c11792f6cc
Echilled <echew140899@gmail.com>
2022-10-24 12:46:07 +0800

    Add files via upload

---
+++ Submission/Mitre Attack and Logs/[Eugene] T1592 Gathering of Victim Host Information/attack_script/passive.py
@@ -0,0 +1,22 @@
+
+import re
+from requests_html import HTMLSession
+from websockets import client
+import html
+
+
+def email_extractor(url):
+	EMAIL_REGEX = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+	session = HTMLSession()
+	r = session.get(url)
+	r.html.render()
+	for re_match in re.finditer(EMAIL_REGEX, r.html.raw_html.decode()):
+		print(re_match.group())
+
+
+def main():
+	email_extractor("http://192.168.91.1/")
+
+
+if __name__ == "__main__":
+	main()
